7bfbfefac6eaa3fdfeeed9bf83b5219c3178f124
jasper <whiteana001@warren.k12.in.us>
2024-08-07 13:23:42 -0400

    Initial Commit

--- src/PoundsToKilos.java
+++ src/PoundsToKilos.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class PoundsToKilos {
+public class PoundsToKilos {
+    public static void main(String[] args) {
+        int lbs = 92;
+
+        double kgs = (lbs * 0.45359237 );
+
+        System.out.println(kgs);
+    }
+
 }
